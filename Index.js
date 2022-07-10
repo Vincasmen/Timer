c39416a4490a3f864e01ff56f64a790d90dff3c9
Vincasmen <vincasmen13@gmail.com>
2022-07-10 20:30:21 +0200

    Added reset button

--- Index.js
+++ Index.js
@@ -1,5 +1,9 @@
 const startBtn = document.querySelector(".startBtn");
+const resetBtn = document.querySelector(".resetBtn");
 
 startBtn.addEventListener("click", function () {
-  console.log("clicked");
+  console.log("START");
+});
+resetBtn.addEventListener("click", function () {
+  console.log("RESET");
 });
